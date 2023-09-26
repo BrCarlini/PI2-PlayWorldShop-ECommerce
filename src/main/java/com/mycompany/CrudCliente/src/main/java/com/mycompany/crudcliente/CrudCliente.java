0f3faf13efc6296fdf25daec1078ab105dfa57a4
Balduscojv <joao.vbsilva30@senacsp.edu.br>
2023-09-26 20:21:57 -0300

    CRUD Cadastro de Cliente

---
+++ src/main/java/com/mycompany/CrudCliente/src/main/java/com/mycompany/crudcliente/CrudCliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudcliente;
+
+/**
+ *
+ * @author baldu
+ */
+public class CrudCliente {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
